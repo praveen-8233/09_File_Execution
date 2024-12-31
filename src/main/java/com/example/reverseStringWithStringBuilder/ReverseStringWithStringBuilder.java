16072bd8d31e60f496d1282800d58795823e97df
praveen-8233 <prasha3613@gmail.com>
2024-12-31 09:44:19 +0530

    ReverseStringWithStringBuilder

--- src/main/java/com/example/reverseStringWithStringBuilder/ReverseStringWithStringBuilder.java
+++ src/main/java/com/example/reverseStringWithStringBuilder/ReverseStringWithStringBuilder.java
@@ -3,16 +3,9 @@ package com.example.reverseStringWithStringBuilder;
 public class ReverseStringWithStringBuilder {
 
     public static void main(String[] args) {
-        String str = "mADAM";
-        if (isPalindrome(str)) {
-            System.out.println(str + " is a palindrome.");
-        } else {
-            System.out.println(str + " is not a palindrome.");
-        }
-    }
-    public static boolean isPalindrome(String str){
-        str = str.toLowerCase();
+        String str = "PRAVEEN";
         String reversedString = new StringBuilder(str).reverse().toString();
-        return str.equals(reversedString);
+        System.out.println(reversedString);
+
     }
 }
